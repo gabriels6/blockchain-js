61388d199205cc9ff85b7079d4ab161102a76a23
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-06 20:44:35 -0300

    V1.0.1 Implementing block mining function and improving block tests

--- test/block.test.js
+++ test/block.test.js
@@ -3,13 +3,61 @@ const expect = require('chai').expect;
 const Block = require('../src/block');
 
 describe('Block class tests', () => {
-    context('Block should be instantiated as newBlock', () => {
+    context('Block "newBlock" has been created', () => {
 
         const timeStamp = Date.now();
         const message = "I like coffee";
-        const nonce = 3;
+        const nonce = 0;
 
         
-        const newBlock = new Block(message,timeStamp,nonce);
+        const newBlock = new Block(nonce,timeStamp,message,3);
+
+        describe('Block should be instantiated as newBlock', () => {
+
+            it('Should contain a message equal to "I like coffee"', () => {
+    
+                expect(newBlock.data).to.be.eq("I like coffee");
+    
+            });
+    
+            it('Should contain a nonce/index equal to 0', () => {
+    
+                expect(newBlock.index).to.be.eq(0);
+    
+            });
+    
+            it('Should contain a previous hash equal to empty string', () => {
+    
+                expect(newBlock.previousHash).to.be.eq("");
+    
+            });
+
+            it('Should contain a difficulty equal to 3', () => {
+                
+                expect(newBlock.difficulty).to.be.eq(3);
+
+            });
+
+            it('Should have a hash different than empty string', () => {
+
+                expect(newBlock.hash).not.to.be.eq("");
+
+            });
+    
+        });
+        
+        describe('Block "newBlock" has been created and mining has been done',  () => {
+
+            it('Should return mining function a nonce higher than 0', async () => {
+    
+                await newBlock.mine();
+
+                expect(newBlock.index).not.to.be.lessThan(0);
+
+            });
+            
+        });
     });
+
+    
 });
--- test/hash.test.js
+++ test/hash.test.js
@@ -4,7 +4,7 @@ const {calculateHash} = require('../src/hash');
 describe('Hash function tests', () => {
     context('Hash function works', () => {
         it('The string "Hello World" should return "A591A6D40BF420404A011733CFB7B190D62C65BF0BCDA32B57B277D9AD9F146E"', () => {
-            expect(calculateHash("Hello World")).to.be.eq("A591A6D40BF420404A011733CFB7B190D62C65BF0BCDA32B57B277D9AD9F146E");
+            expect(calculateHash("Hello World")).to.be.eq("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e");
         });
         
     });
